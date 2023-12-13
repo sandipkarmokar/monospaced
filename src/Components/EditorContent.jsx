c207fd4d872b533c60f66aa3c1ef3533c1bb16e8
sandipkarmokar <skarmokar987@gmail.com>
2023-12-13 12:26:31 +0530

    removed highlight.js instead using monaco-editor

--- src/Components/EditorContent.jsx
+++ src/Components/EditorContent.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-import hljs from "highlight.js";
-import Javascript from "../Language Files/javascript";
 import Editor from "@monaco-editor/react";
 
 function EditorContent() {
-  React.useEffect(() => {
-    hljs.highlightAll();
-  }, []);
+  // React.useEffect(() => {
+  //   hljs.highlightAll();
+  // }, []);
+
+  const jsValue = `
+    const greeter = (name) => {
+      console.log(name);
+    };
+
+    greeter("alex");
+    const greeter = (name) => {
+      console.log(name);
+    };
+
+    greeter("alex");
+    const greeter = (name) => {
+      console.log(name);
+    };
+
+    greeter("alex");
+        `;
   return (
     <div className="h-full">
       <Editor
         className="h-full"
         theme="vs-dark"
         defaultLanguage="javascript"
+        defaultValue={jsValue}
       ></Editor>
-      {/* <Javascript /> */}
-      <link
+      {/* <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark-reasonable.css"
       />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-      <script>hljs.highlightAll();</script>
+      <script>hljs.highlightAll();</script> */}
     </div>
   );
 }
