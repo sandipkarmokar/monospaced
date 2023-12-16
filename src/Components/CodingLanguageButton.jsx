17e454e880560230adfb109b982f7b06afc9b861
sandipkarmokar <skarmokar987@gmail.com>
2023-12-16 17:26:46 +0530

    fixed the issue of coding language not highlighting by removing the coditional rendering inside the button, now there are two different button which render on the basis of whether isSelected is true or false

--- src/Components/CodingLanguageButton.jsx
+++ src/Components/CodingLanguageButton.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 
 function CodingLanguageButton({ setCodingLanguage, name, id, isSelected }) {
+  // return (
+  // <button
+  //   key={id}
+  //   className={`bg-${
+  //     isSelected ? "dark_blue-200" : "dark_blue-300"
+  //   } text-white border-[#012A45] border-2 px-6 py-2`}
+  //   onClick={() => setCodingLanguage(name)}
+  // >
+  //   {name}
+  // </button>
+  // );
+
+  // For some reason having a single button and using conditional rendering on that button to display different states doesnt work properly with tailwind
   return (
-    <button
-      key={id}
-      className={`bg-${
-        isSelected ? "dark_blue-200" : "dark_blue-300"
-      } text-white border-[#012A45] border-2 px-6 py-2`}
-      onClick={() => setCodingLanguage(name)}
-    >
-      {name}
-    </button>
+    <>
+      {isSelected ? (
+        <button
+          key={id}
+          className="bg-dark_blue-200
+       text-white border-[#012A45] border-2 px-6 py-2"
+          onClick={() => setCodingLanguage(name)}
+        >
+          {name}
+        </button>
+      ) : (
+        <button
+          key={id}
+          className="bg-dark_blue-300
+       text-white border-[#012A45] border-2 px-6 py-2"
+          onClick={() => setCodingLanguage(name)}
+        >
+          {name}
+        </button>
+      )}
+    </>
   );
 }
 
